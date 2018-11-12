3d44d20ec33cef576dd142e79549d8dd3a5068c4
Januario Carreiro <jjc70@duke.edu>
2018-11-11 19:05:44 -0500

    APT Quiz 2

--- APTs/APT4/src/Leaves.java
+++ APTs/APT4/src/Leaves.java
@@ -1,7 +1,22 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Leaves {
     private ArrayList<Integer> myList = new ArrayList<>();
+
+    private void makeList(TreeNode t) {
+        if (t == null) return;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(t);
+        while(!q.isEmpty()) {
+            TreeNode tree = q.remove();
+            if (tree.left != null) q.add(tree.left);
+            if (tree.right != null) q.add(tree.right);
+            if (tree.left == null && tree.right == null) myList.add(tree.info);
+        }
+    }
 
     private void getLeavesHelper(TreeNode t) {
         if (t == null) return;
@@ -14,7 +29,7 @@ public class Leaves {
     }
 
     public int[] getLeaves(TreeNode t) {
-        getLeavesHelper(t);
+        makeList(t);
         int[] count = new int[myList.size()];;
         for (int i = 0; i < count.length; i++) {
             count[i] = myList.get(i);
--- APTs/APT4/src/LevelLabel.java
+++ APTs/APT4/src/LevelLabel.java
@@ -1,12 +1,13 @@
 public class LevelLabel {
-    private int level(TreeNode t, int level) {
-        if (t == null) return 0;
-        if (t.left != null && t.right != null) return level;
-        return Math.max(level(t.left, level + 1), level(t.right, level + 1)) + 1;
+    private void level(TreeNode t, int level) {
+        if (t == null) return;
+        t.info = level;
+        if (t.left != null) level(t.left, level + 1);
+        if (t.right != null) level(t.right, level + 1);
     }
 
     public TreeNode recharge(TreeNode t) {
-        if (t == null) return null;
-        return new TreeNode(level(t, 1), recharge(t.left), recharge(t.right));
+        level(t, 1);
+        return t;
     }
 }
